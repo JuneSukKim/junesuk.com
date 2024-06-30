0e0728b179109c8df6805838e221cbf69d028fd3
JuneSukKim <junesuk04@hufs.ac.kr>
2024-06-30 22:59:56 +0900

    first

---
+++ junesuk.com/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          일기장을 포함한 메모장 만들기
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
